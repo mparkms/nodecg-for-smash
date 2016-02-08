0a6fc693c77cfb474a53c3935978ee8de4673029
mparkms <mikeparkms@gmail.com>
2016-02-07 20:06:59 -0800

    Finished updating crew-roster

--- dashboard/js/crew-roster.js
+++ dashboard/js/crew-roster.js
@@ -1,58 +1,65 @@
-'use strict';
+(function() {
+	'use strict';
+	
+	console.log('crew-roster.js');
 
-var $panel = $bundle.filter('.ssbm-crew-roster');
+	var $update = $('.ssbm-crew-update');
+	var $hide = $('.ssbm-crew-hide');
 
-var $update = $panel.find('.ssbm-crew-update');
-var $hide = $panel.find('.ssbm-crew-hide');
+	$update.click(function () {
+		nodecg.sendMessage('ssbmCrewUpdate', updateData());
+	});
 
-$update.click(function () {
-	nodecg.sendMessage('ssbmCrewUpdate', updateData());
-});
+	$hide.click(function() {
+		nodecg.sendMessage('ssbmCrewHide', 0);
+	});
 
-$hide.click(function() {
-	nodecg.sendMessage('ssbmCrewHide', 0);
-})
+	function updateData() {
+		var roster1 = getRoster(1);
+		var roster2 = getRoster(2);
+		var teamNamesStock = getTeamNamesStock();
+		var kos1 = getKOs(1);
+		var kos2 = getKOs(2);
 
-function updateData() {
-	var roster1 = getRoster(1);
-	var roster2 = getRoster(2);
-	var teamNamesStock = getTeamNamesStock();
-	var kos1 = getKOs(1);
-	var kos2 = getKOs(2);
-
-	return {
-		'roster1': roster1,
-		'roster2': roster2,
-		'teamNamesStock': teamNamesStock,
-		'kos1': kos1,
-		'kos2': kos2
-	}
-}
-
-function getRoster(team) {
-	var roster = [];
-
-	for(var i = 1; i <= 10; i++) {
-		roster.push($('#ssbm-crew' + team + '-p' + i).val());
+		return {
+			'roster1': roster1,
+			'roster2': roster2,
+			'teamNamesStock': teamNamesStock,
+			'kos1': kos1,
+			'kos2': kos2
+		}
 	}
 
-	return roster;
-}
+	function getRoster(team) {
+		var roster = [];
+		var members = $('#roster' + team + ' > crew-member');
 
-function getTeamNamesStock() {
-	return {
-		'team1name': $('#ssbm-crew1-name').val(),
-		'team1stock': $('#ssbm-crew1-stocks').val(),
-		'team2name': $('#ssbm-crew2-name').val(),
-		'team2stock': $('#ssbm-crew2-stocks').val()
-	}
-}
+		for(var i = 0; i < 10; i++) {
+			roster.push(members.get(i).getTag());
+		}
 
-function getKOs(team) {
-	var kos = [];
-	for(var i = 1; i <= 10; i++) {
-		kos.push($('#ssbm-crew' + team + '-ko' + i).is(':checked'));
+		return roster;
 	}
 
-	return kos;
-}
+	function getTeamNamesStock() {
+		var team1 = $('crew-score').get(0);
+		var team2 = $('crew-score').get(1);
+		return {
+			'team1name': team1.getName(),
+			'team1stock': team1.getScore(),
+			'team2name': team2.getName(),
+			'team2stock': team2.getScore()
+		}
+	}
+
+	function getKOs(team) {
+		var kos = [];
+		var members = $('#roster' + team + ' > crew-member');
+
+		for(var i = 0; i < 10; i++) {
+			kos.push(members.get(i).getKO());
+		}
+
+		return kos;
+	}
+})();
