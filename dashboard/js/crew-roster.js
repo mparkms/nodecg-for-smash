18a55f519eee42273be435a38151344c7d9d3905
mparkms <mikeparkms@gmail.com>
2016-02-07 21:27:13 -0800

    Removed extraneous testing stuff

--- dashboard/js/crew-roster.js
+++ dashboard/js/crew-roster.js
@@ -1,7 +1,5 @@
 (function() {
 	'use strict';
-	
-	console.log('crew-roster.js');
 
 	var $update = $('.ssbm-crew-update');
 	var $hide = $('.ssbm-crew-hide');
--- dashboard/js/scene-change.js
+++ dashboard/js/scene-change.js
@@ -1,52 +1,51 @@
 (function() {
-'use strict';
+	'use strict';
 
-// var OBSRemote = require('obs-remote');
-// var obs = new OBSRemote();
-// obs.connect('localhost');
-console.log('scene-change.js');
+	// var OBSRemote = require('obs-remote');
+	// var obs = new OBSRemote();
+	// obs.connect('localhost');
 
-var $sceneUpdate = $('.ssbm-scene-update');
-var $changeNoTrans = $('.ssbm-scene-change-no-trans');
-var $changeTrans = $('.ssbm-scene-change-trans');
-var $changeTransDown = $('.ssbm-scene-change-trans-down');
-var $transUp = $('.ssbm-trans-up');
+	var $sceneUpdate = $('.ssbm-scene-update');
+	var $changeNoTrans = $('.ssbm-scene-change-no-trans');
+	var $changeTrans = $('.ssbm-scene-change-trans');
+	var $changeTransDown = $('.ssbm-scene-change-trans-down');
+	var $transUp = $('.ssbm-trans-up');
 
-$sceneUpdate.click(function() {
-	nodecg.sendMessage('ssbmScenesUpdate', 0, function(result) {
-		updateScenes(result);
+	$sceneUpdate.click(function() {
+		nodecg.sendMessage('ssbmScenesUpdate', 0, function(result) {
+			updateScenes(result);
+		});
 	});
-});
 
-$changeNoTrans.click(function() {
-	// console.log(document.querySelector('#scenes').getSelected());
-	nodecg.sendMessage('ssbmSceneChange', document.querySelector('#scenes').getSelected());
-});
+	$changeNoTrans.click(function() {
+		// console.log(document.querySelector('#scenes').getSelected());
+		nodecg.sendMessage('ssbmSceneChange', document.querySelector('#scenes').getSelected());
+	});
 
-$changeTrans.click(function() {
-	nodecg.sendMessage('ssbmDropIn');
-	setTimeout(function() {	nodecg.sendMessage('ssbmSceneChange', document.querySelector('#scenes').getSelected()); }, 800);
-	setTimeout(function() { nodecg.sendMessage('ssbmDropOut'); }, 1000);
-});
+	$changeTrans.click(function() {
+		nodecg.sendMessage('ssbmDropIn');
+		setTimeout(function() {	nodecg.sendMessage('ssbmSceneChange', document.querySelector('#scenes').getSelected()); }, 800);
+		setTimeout(function() { nodecg.sendMessage('ssbmDropOut'); }, 1000);
+	});
 
-$changeTransDown.click(function() {
-	nodecg.sendMessage('ssbmDropIn');
-	setTimeout(function() {	nodecg.sendMessage('ssbmSceneChange', document.querySelector('#scenes').getSelected()); }, 800);
-});
+	$changeTransDown.click(function() {
+		nodecg.sendMessage('ssbmDropIn');
+		setTimeout(function() {	nodecg.sendMessage('ssbmSceneChange', document.querySelector('#scenes').getSelected()); }, 800);
+	});
 
-$transUp.click(function() {
-	nodecg.sendMessage('ssbmDropOut');
-});
+	$transUp.click(function() {
+		nodecg.sendMessage('ssbmDropOut');
+	});
 
-function setToName(element, index, array) {
-	array[index] = element.name;
-}
+	function setToName(element, index, array) {
+		array[index] = element.name;
+	}
 
-function updateScenes(scenes) {
-	// $('#ssbm-scenes').empty();
-	// scenes.forEach(updateSelect);
-	scenes.forEach(setToName);
-	// console.log(scenes);
-	document.querySelector('#scenes').setScenes(scenes);
-}
+	function updateScenes(scenes) {
+		// $('#ssbm-scenes').empty();
+		// scenes.forEach(updateSelect);
+		scenes.forEach(setToName);
+		// console.log(scenes);
+		document.querySelector('#scenes').setScenes(scenes);
+	}
 })();
