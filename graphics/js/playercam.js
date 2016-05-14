a831c51d71a40fdb2e965765b411a8a4528dc34a
mparkms <mikeparkms@gmail.com>
2016-05-13 22:26:02 -0700

    Playercam -> match info

--- graphics/js/playercam.js
+++ graphics/js/playercam.js
@@ -4,7 +4,7 @@ $(function() {
 	nodecg.listenFor('ssbmPlayercam', updateLabel);
 
 	function updateLabel(data) {
-		$('#container').animate({top: "100%"}, {duration: 1000, complete: function() {
+		$('#container').animate({top: "-100%"}, {duration: 1000, complete: function() {
 			$('#text').text(data);
 		}});
 		$('#container').animate({top: "0%"}, 1000);
